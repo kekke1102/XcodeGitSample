958e2e0ea32c1354c8a51536e225e7a845372922
kekke1102 <Keisuke@kAir.local>
2013-10-10 11:09:51 +0900

    Initial Commit

---
+++ XcodeGitSample/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  XcodeGitSample
+//
+//  Created by Keisuke Fukushima on 2013/10/10.
+//  Copyright (c) 2013年 keisuke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
